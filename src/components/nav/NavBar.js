123ef4bd02caee48f519320a6a2feddb2b70eeb0
Corentin Desfarges <corentin.desfarges@gmail.com>
2023-08-07 20:44:53 -0400

    fix navbar on mobile devices

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -11,7 +11,7 @@ import SocialNavItem from "./SocialNavItem"
 
 export default function NavBar() {
   return (
-    <div className="flex-col items-center justify-between md:flex fixed left-0 top-full z-50 w-full md:w-16 md:sticky md:top-20 md:h-[calc(100vh-theme(space.28))] md:translate-y-0 -translate-y-16">
+    <div className="flex-col items-center justify-between md:flex fixed left-0 bottom-0 z-50 w-full md:w-16 md:sticky md:top-20 md:h-[calc(100vh-theme(space.28))]">
       <div className="grid grid-cols-4 w-full bg-[#0a1838] md:block">
         <NavItem
           label={"Qui je suis"}
@@ -33,7 +33,7 @@ export default function NavBar() {
         <NavItem label={"Projets"} icon={<Realisations />} href={"/projets"} />
         <NavItem label={"Contact"} icon={<Contact />} href={"/contact"} />
       </div>
-      <div className="flex flex-col flex-wrap">
+      <div className="hidden md:flex flex-col flex-wrap">
         <SocialNavItem label={"GitHub"} icon={<GitHub />} href="https://github.com/corentindesfarges" />
         <SocialNavItem label={"LinkedIn"} icon={<LinkedIn />} href="https://www.linkedin.com/in/corentin-desfarges/" />
       </div>
